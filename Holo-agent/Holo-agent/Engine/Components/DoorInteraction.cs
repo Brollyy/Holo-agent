d0bc8ff13198fc783c758849bb0cf4134c381d78
whiteraav <raavakan@gmail.com>
2016-04-19 10:43:44 +0200

    Initial DoorInteraction

---
+++ Holo-agent/Holo-agent/Engine/Components/DoorInteraction.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Engine;
+using Engine.Components;
+using Engine.Utilities;
+
+namespace Engine.Components
+{
+    class DoorInteraction : Interaction 
+    {
+        public override void Interact(GameObject go)
+        {
+            Owner.GlobalRotation = Quaternion.CreateFromYawPitchRoll(MathHelper.ToRadians(225), MathHelper.ToRadians(270), 0);
+        }
+
+    }
+}
